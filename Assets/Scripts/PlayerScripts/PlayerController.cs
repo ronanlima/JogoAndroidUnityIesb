6838aff08a63f4f8aedad5f25dc135f1f936a1d9
Wanderlei B. Oliveira <wanderlei.brandao@gmail.com>
2020-06-22 00:58:46 -0300

    Configurando Player.
    Criando Script para controlar Player.

---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Propriedades de controle
+    public float moveSpeed =10f;
+    public float jumpForce = 10f;
+    public float slideForce = 6;
+    private bool isJump = false;
+    private float inJump = 0;
+    private float inSlide = 0;
+
+
+    //Recursos usados no GameObject
+    private SpriteRenderer playerSpriteRenderer;
+    private Rigidbody2D playerRigidBody;
+    private Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Mover();
+            Jump();
+            Slider();
+    }
+
+    void Mover()
+    {
+        float cmd = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(cmd, 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+        playerAnimator.SetBool("isRun", cmd!=0);
+
+        if(cmd > 0 ){
+            playerSpriteRenderer.flipX = false;
+        }
+
+        if(cmd < 0 ){
+            playerSpriteRenderer.flipX = true;
+        }
+
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && inJump <= 0){
+            playerRigidBody.AddForce(
+                new Vector2(0f, jumpForce),
+                ForceMode2D.Impulse
+            );
+            isJump = true;
+            playerAnimator.SetBool("isJump", isJump);
+            inJump = jumpForce;
+        }
+
+        if(inJump <= 0){
+             isJump = false;
+             playerAnimator.SetBool("isJump", isJump);
+        }
+
+        if(inJump>0){
+            inJump -= Time.deltaTime *(jumpForce/2);
+        }
+    }
+
+    void Slider()
+    {
+        if(Input.GetButtonDown("Fire1") && inSlide <= 0 && !isJump){
+            playerAnimator.SetBool("isSlide", true);
+            inSlide = slideForce;
+        }
+
+        if(inSlide <= 0){
+             playerAnimator.SetBool("isSlide", false);
+        }
+
+        if(inSlide>0){
+            float direction = (playerSpriteRenderer.flipX)?-1f:1f;
+            inSlide -= 1.7f * Time.deltaTime * slideForce;
+            Vector3 movement = new Vector3(direction, 0f, 0f);
+            transform.position += movement * Time.deltaTime * slideForce;
+        }
+    }
+}
