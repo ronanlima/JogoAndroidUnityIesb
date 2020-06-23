dcf9f6092b9653c24401a6f7dde1b94760bce3a8
ronan.lima <ronan.lima@sicoob.com.br>
2020-06-22 22:50:25 -0300

    Alterando script - player morto
    Criando estrutura de controle para quando player morrer

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
 
     //Propriedades de controle
     public float moveSpeed =10f;
-    public float jumpForce = 10f;
+    public float jumpForce = 0.5f;
     public float slideForce = 6;
+    public int life = 100;
     private bool isJump = false;
+    private bool isDead = false;
     private float inJump = 0;
     private float inSlide = 0;
 
@@ -20,23 +23,38 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnimator;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         playerSpriteRenderer = GetComponent<SpriteRenderer>();
         playerRigidBody = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
             Mover();
             Jump();
             Slider();
     }
 
-    void Mover()
-    {
+    bool playerDead() {
+        Console.Write("playerDead");
+        if (life < 1) {
+            Console.Write("playerDead < 1");
+            isDead = true;
+            playerAnimator.SetBool("tgrDead", isDead);
+        }
+        if (isDead) {
+            Console.Write("playerDead - está morto");
+            return true;
+        }
+        return false;
+    }
+
+    void Mover() {
+        Console.Write("Mover");
+        if (playerDead()) {
+            return;
+        }
         float cmd = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(cmd, 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
@@ -53,8 +71,11 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void Jump()
-    {
+    void Jump() {
+        Console.Write("Jump");
+        if (playerDead()) {
+            return;
+        }
         if(Input.GetButtonDown("Jump") && inJump <= 0){
             playerRigidBody.AddForce(
                 new Vector2(0f, jumpForce),
@@ -75,8 +96,11 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void Slider()
-    {
+    void Slider() {
+        Console.Write("Slider");
+        if (playerDead()) {
+            return;
+        }
         if(Input.GetButtonDown("Fire1") && inSlide <= 0 && !isJump){
             playerAnimator.SetBool("isSlide", true);
             inSlide = slideForce;
