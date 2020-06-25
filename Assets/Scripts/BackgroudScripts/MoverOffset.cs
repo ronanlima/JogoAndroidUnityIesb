8606dcc34291d779d99c8e3cadd5066a30db2d9e
Wanderlei B. Oliveira <wanderlei.brandao@gmail.com>
2020-06-24 23:36:37 -0300

    Criando estrutura de funto de tela.
    Criando estrutura de funto de tela.

---
+++ Assets/Scripts/BackgroudScripts/MoverOffset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverOffset : MonoBehaviour
+{
+    private Material currentMaterial;
+    private float offsetX = 0f;
+    private float offsetY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            offsetX += 0.002f * Time.deltaTime;
+            currentMaterial.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+    }
+}
