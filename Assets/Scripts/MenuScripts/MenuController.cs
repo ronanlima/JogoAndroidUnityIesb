b490bf13dc9437956c997fdb1756c0af2dbee034
Wanderlei B. Oliveira <wanderlei.brandao@gmail.com>
2020-06-29 00:49:14 -0300

    Criando Menu Inicial
    Criando Menu Inicial

---
+++ Assets/Scripts/MenuScripts/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+
+    public void LoadScene(string scene){
+        SceneManager.LoadScene(scene);
+    }
+
+    public void FecharJogo(){
+        Application.Quit();
+    }
+
+}
