eb1f62c446aba29be71770c102bf90d6caa28e88
Wanderlei B. Oliveira <wanderlei.brandao@gmail.com>
2020-07-06 01:32:57 -0300

    Adicionando Tiro
    Adicionando Tiro

---
+++ Assets/Scripts/PlayerScripts/PlayerTiro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTiro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float tiroSpeed = 10f;
+    private float tiroDestroy = 1.5f;
+    void Start()
+    {
+        Destroy(gameObject, tiroDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * tiroSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(){
+        //Destroy(gameObject);
+    }
+}
