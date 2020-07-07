1f378a537304e7114f0a48f506cc85e00e75a59d
Wanderlei B. Oliveira <wanderlei.brandao@gmail.com>
2020-07-07 19:50:40 -0300

    MenuFase02
    MenuFase02

---
+++ Assets/Scripts/InimigosCommum/SpawnInimigo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SpawnInimigo : MonoBehaviour
+{
+    public GameObject inimigoObject;
+    public Transform inimigoSpawn;// Start is called before the first frame update
+
+    public float timeSpawn = 10;
+    public int contTimeSpawn = 5; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeSpawn<=0){
+            GerarInimigo();
+            timeSpawn = 10f;
+        }else{
+            timeSpawn -= 1 * Time.deltaTime;
+        }
+
+        Debug.Log(timeSpawn);
+    }
+
+    void GerarInimigo(){
+        GameObject zumb = Instantiate(inimigoObject, inimigoSpawn.position, inimigoSpawn.rotation);
+    }
+}
